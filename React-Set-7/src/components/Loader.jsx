dacca72894d7f67ed1d64787e1b69eecce557499
Sakib008 <mdshakib1902@gmail.com>
2025-04-20 16:12:36 +0530

    fix : make a loader for loading

---
+++ React-Set-7/src/components/Loader.jsx
@@ -0,0 +1,10 @@
+
+const Loader = () => {
+  return (
+    <div className="flex justify-center items-center h-[50vh]">
+       <p className="text-9xl font-bold animate-ping">Loading...</p>
+    </div>
+  )
+}
+
+export default Loader
