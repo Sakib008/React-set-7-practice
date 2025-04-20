2d8eda2eb27263d77a5801c84163643a7b15cbd9
Sakib008 <mdshakib1902@gmail.com>
2025-04-20 16:11:29 +0530

    fix : plain html to culourful css with tailwindcss

--- React-Set-7/src/App.jsx
+++ React-Set-7/src/App.jsx
@@ -2,39 +2,58 @@ import "./App.css";
 import { NavLink, Route, Routes } from "react-router-dom";
 
 // import { DisplayProducts } from "./Pages/displayProduct";
-// import { DisplayMovies2 } from "./Pages/displaymovies2";
 import { DisplayQuotes } from "./Pages/displayQuotes";
 import { DisplayMovies } from "./Pages/displayMovies";
 import { DisplayWeather } from "./Pages/displayWeather";
-
+// import { DisplayMovies2 } from "./Pages/displaymovies2";
 import { DisplayProduct2 } from "./Pages/displayProducts2";
 
 export default function App() {
   return (
-    <div>
-      <h1>Sakib&apos;s Dev Box</h1>
+    <div className="bg-yellow-400 w-screen h-screen flex ">
+      <div className="w-[80vw] h-[80vh] m-auto flex items-center flex-col justify-start border-4 bg-sky-400 rounded-2xl border-red-300 ">
+        <h1 className="text-3xl font-bold text-white font-fira-code ">
+          Sakib&apos;s Dev Box
+        </h1>
 
-      {/* <DisplayWeather/> */}
-      {/* <DisplayMovies/> */}
-      {/* <DisplayMovies2 /> */}
-      {/* <DisplayQuotes /> */}
-      {/* <DisplayProducts /> */}
+        {/* <DisplayWeather /> */}
+        {/* <DisplayMovies /> */}
+        {/* <DisplayMovies2 /> */}
+        {/* <DisplayQuotes /> */}
+        {/* <DisplayProducts /> */}
 
-      <h1>My Cart</h1>
-      <nav>
-        <NavLink to={"/"}> Weather || </NavLink>
-        <NavLink to={"/product"}> Products || </NavLink>
-        <NavLink to={"/movies"}> Movies || </NavLink>
-        <NavLink to={"/quotes"}> Quotes</NavLink>
-      </nav>
-      <Routes>
-        <Route path="/" element={<DisplayWeather />} />
-        <Route path="/product" element={<DisplayProduct2 />} />
-        <Route path="/movies" element={<DisplayMovies />} />
-        <Route path="/quotes" element={<DisplayQuotes />} />
-      </Routes>
+        <nav className="flex p-2 font-bold text-xl rounded-2xl border-4  border-rose-300 font-fira-code ">
+          <NavLink className="mx-10 cursor-pointer hover:text-red-500" to={"/"}>
+            Weather
+          </NavLink>
+          <NavLink
+            className="mx-10 hover:text-red-500 cursor-pointer"
+            to={"/product"}
+          >
+            Products
+          </NavLink>
+          <NavLink
+            className="mx-10 cursor-pointer hover:text-red-500"
+            to={"/movies"}
+          >
+            Movies
+          </NavLink>
+          <NavLink
+            className="mx-10 cursor-pointer hover:text-red-500"
+            to={"/quotes"}
+          >
+            Quotes
+          </NavLink>
+        </nav>
+        <Routes>
+          <Route path="/" element={<DisplayWeather />} />
+          <Route path="/product" element={<DisplayProduct2 />} />
+          <Route path="/movies" element={<DisplayMovies />} />
+          <Route path="/quotes" element={<DisplayQuotes />} />
+        </Routes>
 
-      {/* <DisplayProduct2 /> */}
+        {/* <DisplayProduct2 /> */}
+      </div>
     </div>
   );
 }
